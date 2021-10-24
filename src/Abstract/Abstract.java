007914128df527cfdb6249254e1f9d19737df933
阿棋 <2932234796@qq.com>
2021-10-24 17:45:36 +0800

    学习了abstract关键字的用法

---
+++ src/Abstract/Abstract.java
@@ -0,0 +1,15 @@
+package Abstract;
+
+/*
+* java abstract关键字的用法：
+* 1.abstract可以用来修饰类和方法
+* 2.abstract方法只能出现于abstract类中，abstract类不能被实例化，只能被继承
+* 3.abstract类中可以出现正常方法，也可以出现abstract方法，当出现abstract方法的时候其子类必须重写abstract类中的abstract方法
+* 4.abstract类和方法的访问一般为public（因为要重写）默认为public，也可以出现protected
+*
+*
+* */
+protected class Abstract {
+
+
+}
